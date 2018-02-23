4dfa17d8620612e5aac58fddb46eee62883eb297
jpcarmona <jpcarmona92@gmail.com>
2018-02-23 21:31:01 +0100

    Avance1 funciones

--- funciones.py
+++ funciones.py
@@ -1,6 +1,21 @@
+def municipio_existe(municipio,arbol):
+	"""Dado un municipio(str)
+	y el arbol(lxml.etree)
+	de 'sevilla.xml',
+	verifica si existe(bool)"""
+	municipios=arbol.xpath('//municipio/text()')
 
+	for muni in municipios:
+		if muni.upper()==municipio.upper():
+			return True
+		else:
+			return False
 
-
-
-
-
+def cod_municipio(municipio,arbol):
+	"""Dado un municipio(str)
+	y el arbol(lxml.etree)
+	de 'sevilla.xml',
+	devuelve codigo(str)"""
+	cod=arbol.xpath('//municipio[contains(text(),municipio)]/@value')
+	codigo=cod[0][-5:]
+	return codigo
