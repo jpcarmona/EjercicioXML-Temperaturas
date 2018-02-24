4a92451b014c4f4ed6adb47ca6d36d0011f59061
jpcarmona <jpcarmona92@gmail.com>
2018-02-24 14:40:27 +0100

    Avance4 tempsevilla

--- tempsevilla.py
+++ tempsevilla.py
@@ -1,10 +1,19 @@
 from lxml import etree
 import time
-from funciones import municipio_existe,cod_municipio,get_url
+from funciones import municipio_existe,cod_municipio,get_url,get_temp
 
 arbolsevilla=etree.parse("sevilla.xml")
 
-if not municipio_existe(municipio,arbol):
-	print("Error")
-else:
-	print(municipio_existe(municipio,arbol))
+print("TEMPERATURAS EN SEVILLA")
+
+municipio=input("Introduce un municipio de Sevilla ("0" para salir): ")
+
+while municipio!="0":
+	if not municipio_existe(municipio,arbol):
+		print("Este municipio no existe en Sevilla")
+	else:
+		municipio=municipio_existe(municipio,arbol)
+		temperaturas=get_temp(get_url(cod_municipio(municipio,arbol)))
+		print("La temperatura máxima en {} es de {}º".format(temperaturas[0],municipio))
+		print("La temperatura mínima en {} es de {}º".format(temperaturas[1],municipio))
+	municipio=input("Introduce un municipio de Sevilla ("0" para salir): ")
